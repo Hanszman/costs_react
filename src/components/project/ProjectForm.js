276a8aebaa15a4790ae8d599617265e861ff1d1a
hanszman <victor.hanszman@hotmail.com>
2023-01-30 17:41:28 -0300

    simulando a API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
 import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({btnText}) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => { // O useEffect é utilizado para executar o useState apenas uma vez e não deixar o react ficar executando infinitas vezes uma requisição na página
+        fetch('http://localhost:5000/categories', { // Utilizado para acesar a API (que nesse projeto foi simulada por meio da biblioteca json-server utilizando o arquivo db.json)
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data);
+        })
+        .catch((err) => console.log(err));
+    }, []); // O segundo parâmetro é o valor inicial, nesse caso, as options vazias []
+
     return (
         <form className={styles.form}>
             <Input
@@ -21,6 +38,7 @@ function ProjectForm({btnText}) {
             <Select
                 text='Selecione a Categoria'
                 name='category_id'
+                options={categories}
             />
             <SubmitButton text={btnText}/>
         </form>
