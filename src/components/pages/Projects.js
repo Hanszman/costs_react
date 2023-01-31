bb04cb4e43723f60e88925a80b1a3282c9d10ebe
hanszman <victor.hanszman@hotmail.com>
2023-01-31 19:18:08 -0300

    criação do componente de loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,10 +4,12 @@ import styles from './Projects.module.css';
 import LinkButton from '../layout/LinkButton';
 import Message from '../layout/Message';
 import Container from '../layout/Container';
+import Loading from '../layout/Loading';
 import ProjectCard from '../project/ProjectCard';
 
 function Projects() {
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
 
     const location = useLocation();
     let message = '';
@@ -17,18 +19,21 @@ function Projects() {
     }
 
     useEffect(() => { // O useEffect é utilizado para executar o useState apenas uma vez e não deixar o react ficar executando infinitas vezes uma requisição na página
-        fetch('http://localhost:5000/projects', { // Utilizado para acesar a API (que nesse projeto foi simulada por meio da biblioteca json-server utilizando o arquivo db.json)
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },    
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data);
-            setProjects(data);
-        })
-        .catch((err) => console.log(err));
+        setTimeout(() => { // Colocado pra mostrar o loading por mais tempo
+            fetch('http://localhost:5000/projects', { // Utilizado para acesar a API (que nesse projeto foi simulada por meio da biblioteca json-server utilizando o arquivo db.json)
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },    
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                setProjects(data);
+                setRemoveLoading(true);
+            })
+            .catch((err) => console.log(err));
+        }, 300)
     }, []); // O segundo parâmetro é o valor inicial, nesse caso vazio []
 
     return (
@@ -49,6 +54,12 @@ function Projects() {
                             category={project.category.name}
                         />
                     ))
+                }
+                {!removeLoading && <Loading/>}
+                {
+                    removeLoading && projects.length === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )
                 }
             </Container>
         </div>
