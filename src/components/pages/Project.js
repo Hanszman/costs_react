783123c59829c8fe8bf8b8246f7b04e719e2c807
hanszman <victor.hanszman@hotmail.com>
2023-02-01 19:04:27 -0300

    criando adição de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,10 +1,12 @@
 import styles from './Project.module.css';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { parse, v4 as uuidv4 } from 'uuid';
 import Loading from '../layout/Loading';
 import Container from '../layout/Container';
 import Message from '../layout/Message';
 import ProjectForm from '../project/ProjectForm';
+import ServiceForm from '../service/ServiceForm';
 
 function Project() {
     const { id } = useParams();
@@ -42,7 +44,7 @@ function Project() {
     function editPost(project) {
         setMessage('');
         
-        // Budget validation
+        // Budget Validation
         if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!');
             setType('error');
@@ -63,6 +65,42 @@ function Project() {
             // Mensagem
             setMessage('Projeto atualizado!');
             setType('success');
+        })
+        .catch((err) => console.log(err));
+    }
+
+    function createService(project) {
+        setMessage('');
+
+        // Last Service
+        const lastService = project.services[project.services.length - 1];
+        lastService.id = uuidv4();
+        const lastServiceCost = lastService.cost;
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+        // Maximun Value Validation
+        if (newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço');
+            setType('error');
+            project.services.pop();
+            return false;
+        }
+
+        // Add Service Cost to Project Total Cost
+        project.cost = newCost;
+
+        // Update Project
+        fetch(`http://localhost:5000/projects/${project.id}`, { // Utilizado para acesar a API (que nesse projeto foi simulada por meio da biblioteca json-server utilizando o arquivo db.json)
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            // Exibir os Serviços
+            console.log(data);
         })
         .catch((err) => console.log(err));
     }
@@ -107,7 +145,11 @@ function Project() {
                                 <div className={styles.project_info}>
                                     {
                                         showServiceForm && (
-                                            <div>Formulário do Serviço</div>
+                                            <ServiceForm
+                                                handleSubmit={createService}
+                                                btnText='Adicionar Serviço'
+                                                projectData={project}
+                                            />
                                         )
                                     }
                                 </div>
